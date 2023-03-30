c96fff7f7587cb72e013e0ee0ef4e310974fa081
Chris Pierson <cpierswim@gmail.com>
2023-03-30 16:53:11 -0400

    NEW: Added the Form to add an entry

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
+import AddEntryForm from "./Components/AddEntry/AddEntryForm";
 import DisplayEntries from "./Components/DisplayEntries/DisplayEntries";
 
 function App() {
   const [entries, setEntries] = useState([{ weight: 175, date: "11-23-2021" }]);
 
+  function addNewEntry(entry) {
+    let tempEntries = [...entries, entry];
+    setEntries(tempEntries);
+  }
+
   return (
     <div>
       <DisplayEntries parentEntries={entries} />
+      <AddEntryForm addNewEntry={addNewEntry} />
     </div>
   );
 }
